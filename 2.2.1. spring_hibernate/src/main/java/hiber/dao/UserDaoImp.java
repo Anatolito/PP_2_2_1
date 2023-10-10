78a32d9c0c5ab1acc81821b68ff93cff9e132d9a
Anatoly <anatolito@yandex.ru>
2023-10-10 16:15:03 +0300

    Change 7 classes,db.properties, add class Car, .gitignore

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -11,19 +11,28 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+
+    @Override
+    public User getCarOwner(String carModel, int carSeries) {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User where userCar.model =:model AND userCar.series =:series");
+        query.setParameter("model", carModel).setParameter("series", carSeries);
+        return query.getSingleResult();
+    }
+
 
 }
